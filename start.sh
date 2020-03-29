1ba8b6d3c0770952380a8ab4e2dd40883e08c175
Go <gokazu@amazon.co.jp>
2020-03-29 15:22:06 +0900

    mjpg tuning

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 source activate prarail
-/usr/local/bin/mjpg_streamer -o "/usr/local/lib/mjpg-streamer/output_http.so -w ./www -p 8080" -i "/usr/local/lib/mjpg-streamer/input_uvc.so -d /dev/video0 -r 640x480 -fps 30 -q 100 -y -n" &
+/usr/local/bin/mjpg_streamer -o "/usr/local/lib/mjpg-streamer/output_http.so -w ./www -p 8080" -i "/usr/local/lib/mjpg-streamer/input_uvc.so -d /dev/video0 -r 640x480 -fps 30 -q 50 -y -n" &
 python prarail.py
 
